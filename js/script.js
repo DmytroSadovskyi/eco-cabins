1e2f8d0b188cd92680ddd24fb878ddb0474b105f
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-12-19 14:24:03 +0200

    add smooth scroll to info section

--- js/script.js
+++ js/script.js
@@ -9,6 +9,20 @@ const swiper = new Swiper('.slider-materials', {
 });
 
 const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+
+const readMoreBtn = document.querySelector('.materials__button');
+const infoSection = document.querySelector('.page__info');
+
+readMoreBtn.addEventListener('click', () => {
+  scrollTo({
+    left: 0,
+    top: infoSection.offsetTop - headerHeight,
+    behavior: 'smooth',
+  });
+
+  console.log(infoSection.offsetTop);
+});
 
 window.addEventListener('scroll', () => {
   window.scrollY > 0 ? header.classList.add('scroll') : header.classList.remove('scroll');
